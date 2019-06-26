42e98721d3efbd80b670d440c79390c3089d7445
Solaner <Solaner@users.noreply.github.com>
2019-06-27 01:05:31 +0200

    3 changes
    a) forcedReload: true // reload the page from the server
    b) var waitingTime1, waitingTimeX // to easaly test different timeout times
    c) Only reload the page, if the 'Last-Modified' time from the server differs from the local document.

--- reload.js
+++ reload.js
@@ -4,20 +4,24 @@
   var loc     = location;
   var host    = loc.hostname + (loc.port ? ':' + loc.port : '');
   var timeout = setTimeout;
-  var ref, check, xhr, lastModified, time;
+  var check, xhr, lastModifiedOnServer, lastModifiedDocument;
   var waitingTime1 = 1000 * 4;      // 4 seconds
   var waitingTimeX = 1000 * 60 * 5; // 5 minutes
+
   if (loc.host === host) {
-    ref = new Date().getTime();
     check = function () {
       xhr = new XMLHttpRequest();
       xhr.onload = function () {
-        lastModified = this.getResponseHeader('Last-Modified');
-        time = new Date(lastModified).getTime();
-        if (time > ref) {
-          loc.reload(true);
-        } else {
-          timeout(check, waitingTimeX);
+        lastModifiedOnServer = this.getResponseHeader('Last-Modified');
+        lastModifiedDocument = document.lastModified;
+        if (lastModifiedOnServer && lastModifiedDocument) {
+          var timeOnServer = new Date(lastModifiedOnServer);
+          var timeDocument = new Date(lastModifiedDocument);
+          if (timeOnServer.getTime() !== timeDocument.getTime()) {
+            loc.reload(true);
+          } else {
+            timeout(check, waitingTimeX);
+          }
         }
       };
       xhr.open('HEAD', loc.href + '?t=' + new Date().getTime(), true);
